7419907fc5701d6c11349e5ef5f8ebce7ae30581
Al <esqmoo@gmail.com>
2023-02-07 13:10:08 +0100

    header: add menu

--- components/header.php
+++ components/header.php
@@ -7,6 +7,9 @@ $result = $db->getResult();
 
 $menus = $result;
 
+/* echo '<pre>';
+var_export($menus);
+echo '</pre>'; */
 ?>
 
 <header class="header_container">
@@ -14,40 +17,19 @@ $menus = $result;
 		<img src="./images/logo/logo_wcg.png" srcset="./images/icons/logo_smal_mobile.png 450w," alt="logo wcg" />
 	</div>
 	<nav class="header_nav_container">
-		<?php 
+		<?php
 		foreach ($menus as $menu) {
-			echo '<a href="" style="background-color:' . $row['RUB_BACKGROUND'] . '>'. $menu['RUB_LIBELLE'].'</a>';
+		?>
+			<a class="menu" href="#" style="background-color:<?php echo $menu['RUB_BACKGROUND']; ?>; color:<?= $menu['RUB_FONT_COLOR'] ?>">
+				<img class="logo-menu" src="./images/logo/<?php echo $menu['med_ressource']; ?>" alt="logo" />
+				<?php echo $menu['RUB_LIBELLE']; ?>
+			</a>
+		<?php
 		}
-			?>
+		?>
 	</nav>
-		<!-- <a href="#" class="color-expertise w20"><img class="logo-menu" src="./images/logo/logo_home.png"
-					alt="logo home" /></a> -->
-
-		<!-- Expertise header -->
-		<!-- <a href="./pages/expertise.html" class="color-expertise"><img class="logo-menu"
-					src="./images/logo/logo_cerveau.png" alt="logo" />
-				Expertises</a> -->
-		<!-- Innovations -->
-		<!-- <a href="./pages/innovation.html" class="color-innovention"><img class="logo-menu"
-					src="./images/logo/logo_ampoule.png" alt="logo" /> Innovations</a> -->
-		<!-- Recrutement -->
-		<!-- <a href="./pages/recrutement.html" class="color-recrutement"><img class="logo-menu"
-					src="./images/logo/logo_loupe.png" alt="logo" />
-				Recrutement</a> -->
-		<!-- Ressources -->
-		<!-- <a href="./pages/ressources.html" class="color-ressource"><img class="logo-menu"
-					src="./images/logo/logo_ressource.png" alt="logo" /> Ressources</a> -->
-		<!-- Secteurs -->
-		<!-- <a href="./pages/secteur.html" class="color-secteur"><img class="logo-menu"
-					src="./images/logo/logo_secteur.png" alt="logo" />
-				Secteurs</a> -->
-		<!-- Actualités -->
-		<!-- <a href="./pages/actualites.html" class="color-actualite"><img class="logo-menu"
-					src="./images/logo/logo_actualite.png" alt="logo" />Actualités</a> -->
-		<!-- Actualités -->
-		<!-- <a href="#" class="color-expertise"><i class="fa-brands fa-tiktok"></i> Actualités</a> -->
-	<!-- </nav> -->
 	<div class="fas fa-bars" id="menu-btn">
-		
 	</div>
+	<style>
+	</style>
 </header>
